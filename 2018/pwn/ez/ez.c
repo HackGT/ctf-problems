832869a1b64b099766d60001a665f96e75da3cfa
Dennis Li <23002167+dli357@users.noreply.github.com>
2018-10-19 18:56:03 -0400

    Fixed ez and added Dockerfile

--- 2018/pwn/ez/ez.c
+++ 2018/pwn/ez/ez.c
@@ -1,4 +1,4 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdint.h>
-int main(){char a[300];printf("%lu\n",(uintptr_t)a);read(0,a,500);}
+int main(){char a[300];printf("%lu\n",(uintptr_t)a);fflush(stdout);read(0,a,500);}
